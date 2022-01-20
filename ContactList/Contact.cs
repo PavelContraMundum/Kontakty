2c36ccdb8b3c1299a7d19e870dd9fe009f5c9762
PavelContraMundum <pavel.sticka@gmail.com>
2022-01-20 21:45:00 +0100

    initial commit

---
+++ ContactList/Contact.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+
+namespace ContactList
+{
+    internal class Contact
+    {
+        private string firstName;
+        private string lastName;
+        private string phone;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        public string Phone
+        {
+            get { return phone; }
+            set {
+                if (value.Length == 10)
+                {
+                    phone = value;
+                }
+                else
+                {
+                    phone = "0000000000";
+                }
+            }
+        }
+
+        public Contact() 
+            {
+            FirstName = "John";
+            LastName = "Doe";
+            Phone = "0000000000";
+            }
+
+        public Contact(string firstName, string lastName, string phone)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Phone = phone;
+        }
+      
+
+    } //end of class
+} // end of namespace
